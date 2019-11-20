29f081ed3bf6db5b23315e06ffa98384216832a0
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2019-11-20 14:45:14 +0200

    Update Helper.php

--- plib/library/Helper.php
+++ plib/library/Helper.php
@@ -20,4 +20,25 @@ class Modules_Microweber_Helper
 		}
 		return implode($pass);
 	}
+	
+	public static function getJsonFromUrl($url, $postfields = [])
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		
+		if (!empty($postfields)) {
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+		}
+		
+		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		
+		$data = curl_exec($ch);
+		
+		curl_close($ch);
+		
+		return @json_decode($data, true);
+	}
 }
