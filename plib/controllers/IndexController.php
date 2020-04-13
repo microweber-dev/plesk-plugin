71d59834fa64eabfb08111f3d3693a46715d7407
Bobkata <selfworksbg@gmail.com>
2020-04-13 15:08:48 +0300

    update

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -953,10 +953,13 @@ class IndexController extends pm_Controller_Action
 
     private function _getAppInstalations()
     {
-
         $data = [];
 
         foreach (Modules_Microweber_Domain::getDomains() as $domain) {
+
+            if (!$domain->hasHosting()) {
+                continue;
+            }
 
             $installationsFind = [];
 
