f52bb9442267648cef2717ad10c39cb736b5303f
Bobkata <selfworksbg@gmail.com>
2020-04-23 11:01:41 +0300

    update

--- plib/hooks/CustomButtons.php
+++ plib/hooks/CustomButtons.php
@@ -11,8 +11,12 @@ class Modules_Microweber_CustomButtons extends pm_Hook_CustomButtons
 
     public function getButtons()
     {
-        return [
-            [
+        $places = [];
+
+        $sessionDomain = pm_Session::getCurrentDomain();
+
+        if ($sessionDomain->hasHosting()) {
+            $places[] = [
                 'place' => [
                     self::PLACE_DOMAIN,
                     self::PLACE_DOMAIN_PROPERTIES
@@ -20,37 +24,42 @@ class Modules_Microweber_CustomButtons extends pm_Hook_CustomButtons
                 'title' => 'Microweber',
                 'description' => 'View all microweber websites.',
                 'icon' => pm_Context::getBaseUrl() . 'images/logo_small.svg',
-                'link' => pm_Context::getBaseUrl() . 'index.php/index/index'
-            ],
-            [
-                'place' => self::PLACE_ADMIN_NAVIGATION,
-                'section' => self::SECTION_NAV_SERVER_MANAGEMENT,
-                'order' => 15,
-                'title' => 'Microweber',
-                'description' => 'Install last version of microweber',
-                'link' => pm_Context::getActionUrl('index', ''),
-                'icon' => pm_Context::getBaseUrl() . 'images/logo_small_white.svg'
-            ],
-            [
-                'place' => self::PLACE_HOSTING_PANEL_TABS,
-                'order' => 15,
-                'title' => 'Microweber',
-                'description' => 'Install last version of microweber',
-                'link' => pm_Context::getActionUrl('index'),
-                'icon' => pm_Context::getBaseUrl() . 'images/logo_small_white.svg',
-            ],
-            [
-                'place' => [
-                    self::PLACE_HOSTING_PANEL_NAVIGATION,
-                    self::PLACE_ADMIN_TOOLS_AND_SETTINGS,
-                    self::PLACE_RESELLER_TOOLS_AND_SETTINGS,
-                ],
-                'order' => 15,
-                'title' => 'Microweber',
-                'description' => 'Install last version of microweber',
-                'link' => pm_Context::getActionUrl('index', 'index'),
-                'icon' => pm_Context::getBaseUrl() . 'images/logo_small_white.svg'
-            ],
+                'link' => pm_Context::getBaseUrl() . 'index.php/index/index',
+            ];
+        }
+
+        $places[] = [
+            'place' => self::PLACE_ADMIN_NAVIGATION,
+            'section' => self::SECTION_NAV_SERVER_MANAGEMENT,
+            'order' => 15,
+            'title' => 'Microweber',
+            'description' => 'Install last version of microweber',
+            'link' => pm_Context::getActionUrl('index', ''),
+            'icon' => pm_Context::getBaseUrl() . 'images/logo_small_white.svg'
         ];
+
+        $places[] = [
+            'place' => self::PLACE_HOSTING_PANEL_TABS,
+            'order' => 15,
+            'title' => 'Microweber',
+            'description' => 'Install last version of microweber',
+            'link' => pm_Context::getActionUrl('index'),
+            'icon' => pm_Context::getBaseUrl() . 'images/logo_small_white.svg',
+        ];
+
+        $places[] = [
+            'place' => [
+                self::PLACE_HOSTING_PANEL_NAVIGATION,
+                self::PLACE_ADMIN_TOOLS_AND_SETTINGS,
+                self::PLACE_RESELLER_TOOLS_AND_SETTINGS,
+            ],
+            'order' => 15,
+            'title' => 'Microweber',
+            'description' => 'Install last version of microweber',
+            'link' => pm_Context::getActionUrl('index', 'index'),
+            'icon' => pm_Context::getBaseUrl() . 'images/logo_small_white.svg'
+        ];
+
+        return $places;
     }
 }
