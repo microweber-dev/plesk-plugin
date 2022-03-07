885a2f4db11eb229d93c890c2c7f68aaf8b7d882
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2022-03-07 14:01:45 +0200

    Create microweber-periodic-task.php

---
+++ plib/scripts/microweber-periodic-task.php
@@ -0,0 +1,17 @@
+<?php
+// Copyright 1999-2016. Parallels IP Holdings GmbH.
+pm_Context::init('microweber-app-installations-scan');
+
+ $taskManager = new pm_LongTask_Manager();
+
+ foreach (Modules_Microweber_Domain::getDomains() as $domain) {
+
+    if (!$domain->hasHosting()) {
+        continue;
+    }
+
+    $task = new Modules_Microweber_TaskDomainAppInstallationScan();
+    $task->setParam('domainId', $domain->getId());
+    $taskManager->start($task, NULL);
+   
+}
