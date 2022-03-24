359d57f73706a604498ec3e18faf65b642caec7b
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-24 14:32:39 +0200

    update

--- plib/controllers/PhpupgradewizardController.php
+++ plib/controllers/PhpupgradewizardController.php
@@ -10,22 +10,16 @@ include_once 'BasepluginController.php';
 
 class PhpupgradewizardController extends BasepluginController
 {
-    private $currentStep = 1;
+    private $currentStep = 0;
     private $maxSteps = 6;
 
     public function init()
     {
         parent::init();
 
-        $this->currentStep = $this->getRequest()->get('step');
-        $this->nextStep = $this->currentStep + 1;
-
-        $this->view->step = $this->currentStep;
-        $this->view->maxSteps = $this->maxSteps;
-        $this->view->nextStep = $this->nextStep;
-        $this->view->nextStepLink = pm_Context::getBaseUrl() . 'index.php/index/phpUpgradeWizard?step=' . ($this->currentStep + 1);
-
+        $this->_generateSteps();
     }
+
     public function indexAction()
     {
         $this->view->pageTitle = $this->_moduleName . ' - PHP Upgrade wizard';
@@ -34,10 +28,44 @@ class PhpupgradewizardController extends BasepluginController
         $this->view->headScript()->appendFile(pm_Context::getBaseUrl() . 'js/php-upgrade-wizard.js');
     }
 
+
+    public function step1Action()
+    {
+        $this->currentStep = 1;
+        $this->_generateSteps();
+
+    }
+
+
+    public function step2Action()
+    {
+        $this->currentStep = 2;
+        $this->_generateSteps();
+
+    }
+
+    public function step3Action()
+    {
+        $this->currentStep = 3;
+        $this->_generateSteps();
+
+    }
+
     public function getoutdateddomainsAction()
     {
         $status = Modules_Microweber_Helper::canIUpdateNewVersionOfApp();
         $this->_helper->json($status);
+    }
+
+    private function _generateSteps()
+    {
+        $this->nextStep = $this->currentStep + 1;
+
+        $this->view->currentStep = $this->currentStep;
+        $this->view->maxSteps = $this->maxSteps;
+        $this->view->nextStep = $this->nextStep;
+        $this->view->nextStepLink = pm_Context::getBaseUrl() . 'index.php/phpupgradewizard/step-' . ($this->currentStep + 1);
+
     }
 
 }
