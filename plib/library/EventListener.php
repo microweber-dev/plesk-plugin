3f7ca1a24ecd912fd1e8c27b286c11b75c6b1961
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-29 11:39:20 +0300

    update

--- plib/library/EventListener.php
+++ plib/library/EventListener.php
@@ -8,104 +8,92 @@
 
 class Modules_Microweber_EventListener implements EventListener
 {
+    public function filterActions()
+    {
+        return [
+            'phys_hosting_create',
+            'phys_hosting_update'
+        ];
+    }
 
-	public function handleEvent($objectType, $objectId, $action, $oldValue, $newValue)
-	{
-		
-		if ($action == 'domain_delete' || $action == 'domain_alias_delete' || $action == 'site_delete') {
+    public function handleEvent($objectType, $objectId, $action, $oldValue, $newValue)
+    {
+        $domain = new pm_Domain($objectId);
+        $planItems = $domain->getPlanItems();
 
-            $domain = new pm_Domain($objectId);
+        if (is_array($planItems) && count($planItems) > 0 && (in_array("microweber", $planItems) || in_array("microweber_without_shop", $planItems) || in_array("microweber_lite", $planItems))) {
 
-            $taskManager = new pm_LongTask_Manager(); 
+            if ($action == 'phys_hosting_create') {
 
-            $task = new Modules_Microweber_TaskDomainAppInstallationScan();
-            $task->setParam('domainId', $domain->getId());
-            $taskManager->start($task, NULL);
+                // Install microweber
+                $this->_installMicroweber($domain, $newValue);
 
-            $task = new Modules_Microweber_TaskDomainAppInstallationCount();
-            $taskManager->start($task, NULL);
+                // Enable or disable shop
+                $this->_updateMicroweber($domain, $newValue);
+            }
 
+            if ($action == 'phys_hosting_update') {
+                $this->_updateMicroweber($domain, $newValue);
+            }
         }
 
-		if ($action == 'phys_hosting_create' || $action == 'phys_hosting_update') {
+    }
 
-			$domain = new pm_Domain($objectId);
-			$planItems = $domain->getPlanItems();
-			
-			if (is_array($planItems) && count($planItems) > 0 && (in_array("microweber", $planItems) || in_array("microweber_without_shop", $planItems) || in_array("microweber_lite", $planItems))) {
-			
-				if ($action == 'phys_hosting_create') {
+    private function _updateMicroweber($domain, $newValues)
+    {
+        $planItems = $domain->getPlanItems();
 
-				    // Install microweber
-					$this->_installMicroweber($domain, $newValue);
-					
-					// Enable or disable shop
-					$this->_updateMicroweber($domain, $newValue);
-				}
-				
-				if ($action == 'phys_hosting_update') {
-					$this->_updateMicroweber($domain, $newValue);
-				}
-				
-			}
-		}
-	}
-	
-	private function _updateMicroweber($domain, $newValues)
-	{
-		$planItems = $domain->getPlanItems();
-		
-		if (is_array($planItems) && count($planItems) > 0) {
-			
-			$microweber = new Modules_Microweber_Update();
-			$microweber->setDomainId($domain->getId());
-			
-			if (in_array("microweber", $planItems)) {
-				$microweber->setShopActive(true);
-			}
-			
-			if (in_array("microweber_without_shop", $planItems)) {
-				$microweber->setShopActive(false);
-			}
-			
-			if (in_array("microweber_lite", $planItems)) {
-				$microweber->setShopActive(false);
-			}
-			
-			$microweber->update();
-		}
-		
-	}
-	
-	private function _installMicroweber($domain, $newValues)
-	{
-		try {
-			$whmcsConnector = new Modules_Microweber_WhmcsConnector();
-			$whmcsConnector->setDomainName($domain->getName());
-			$selectedTemplate = $whmcsConnector->getSelectedTemplate();
-			
-			$newInstallation = new Modules_Microweber_Install();
-			$newInstallation->setDomainId($domain->getId());
-			$newInstallation->setType(pm_Settings::get('installation_type'));
-			$newInstallation->setDatabaseDriver(pm_Settings::get('installation_database_driver'));
-			$newInstallation->setUsername($newValues['System User']);
-			$newInstallation->setEmail($newValues['System User']);
-			$newInstallation->setPassword($newValues['System User Password']);
-			
-			if (! empty($selectedTemplate)) {
-				$newInstallation->setTemplate($selectedTemplate);
-			}
-			
-			$newInstallation->run();
-			
-		} catch (pm_Exception $e) {
-			pm_Settings::set('domain_issue_' . $domain->getId(), pm_Locale::lmsg('microweberError', [
-				'domain' => $domain->getDisplayName(),
-				'package' => $domain->getPlanItems()[0],
-				'error' => $e->getMessage()
-			]));
-		}
-	}
+        if (is_array($planItems) && count($planItems) > 0) {
+
+            $microweber = new Modules_Microweber_Update();
+            $microweber->setDomainId($domain->getId());
+
+            if (in_array("microweber", $planItems)) {
+                $microweber->setShopActive(true);
+            }
+
+            if (in_array("microweber_without_shop", $planItems)) {
+                $microweber->setShopActive(false);
+            }
+
+            if (in_array("microweber_lite", $planItems)) {
+                $microweber->setShopActive(false);
+            }
+
+            $microweber->update();
+        }
+
+    }
+
+    private function _installMicroweber($domain, $newValues)
+    {
+        try {
+            $whmcsConnector = new Modules_Microweber_WhmcsConnector();
+            $whmcsConnector->setDomainName($domain->getName());
+            $selectedTemplate = $whmcsConnector->getSelectedTemplate();
+
+            $newInstallation = new Modules_Microweber_Install();
+            $newInstallation->setDomainId($domain->getId());
+            $newInstallation->setType(pm_Settings::get('installation_type'));
+            $newInstallation->setDatabaseDriver(pm_Settings::get('installation_database_driver'));
+            $newInstallation->setUsername($newValues['System User']);
+            $newInstallation->setEmail($newValues['System User']);
+            $newInstallation->setPassword($newValues['System User Password']);
+
+            if (!empty($selectedTemplate)) {
+                $newInstallation->setTemplate($selectedTemplate);
+            }
+
+            $newInstallation->run();
+
+        } catch (pm_Exception $e) {
+            pm_Settings::set('domain_issue_' . $domain->getId(), pm_Locale::lmsg('microweberError', [
+                'domain' => $domain->getDisplayName(),
+                'package' => $domain->getPlanItems()[0],
+                'error' => $e->getMessage()
+            ]));
+        }
+    }
 }
 
 return new Modules_Microweber_EventListener();
