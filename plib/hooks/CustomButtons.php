89c8b764d987615458aaa01b0e0addba6a2548bf
Bozhidar Slaveykov <bobi@microweber.com>
2022-05-09 12:18:23 +0300

    Update CustomButtons.php

--- plib/hooks/CustomButtons.php
+++ plib/hooks/CustomButtons.php
@@ -35,23 +35,22 @@ class Modules_Microweber_CustomButtons extends pm_Hook_CustomButtons
             $showButtons = true;
         }
 
-        if (!$showButtons) {
-            return [];
-        }
-
         $places = [];
-        $places[] = [
-            'place' => [
-                self::PLACE_DOMAIN,
-                self::PLACE_DOMAIN_PROPERTIES,
-                self::PLACE_RESELLER_TOOLS_AND_SETTINGS
-            ],
-            'title' => Modules_Microweber_WhiteLabel::getBrandName(),
-            'description' => 'View all ' . Modules_Microweber_WhiteLabel::getBrandName() . ' websites.',
-            'icon' => Modules_Microweber_WhiteLabel::getBrandAppIcon(),
-            'link' => pm_Context::getBaseUrl() . 'index.php/index/index',
-            'contextParams' => true
-        ];
+
+        if ($showButtons) {
+            $places[] = [
+                'place' => [
+                    self::PLACE_DOMAIN,
+                    self::PLACE_DOMAIN_PROPERTIES,
+                    self::PLACE_RESELLER_TOOLS_AND_SETTINGS
+                ],
+                'title' => Modules_Microweber_WhiteLabel::getBrandName(),
+                'description' => 'View all ' . Modules_Microweber_WhiteLabel::getBrandName() . ' websites.',
+                'icon' => Modules_Microweber_WhiteLabel::getBrandAppIcon(),
+                'link' => pm_Context::getBaseUrl() . 'index.php/index/index',
+                'contextParams' => true
+            ];
+        }
 
         $places[] = [
             'place' => self::PLACE_ADMIN_NAVIGATION,
@@ -62,7 +61,7 @@ class Modules_Microweber_CustomButtons extends pm_Hook_CustomButtons
             'link' => pm_Context::getActionUrl('index', ''),
             'icon' => Modules_Microweber_WhiteLabel::getBrandInvertIcon()
         ];
-
+        
         $places[] = [
             'place' => [self::PLACE_HOSTING_PANEL_TABS],
             'order' => 15,
