5624bea8c7379c4891b9ffe7cc7831be6e8eb815
Bozhidar Slaveykov <bobi@microweber.com>
2022-07-04 20:13:03 +0300

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,32 +1,10 @@
-Html Webpack Plugin:
-<pre>
-  Error: Child compilation failed:
-  Module build failed (from /Users/bobi/PhpstormProjects/takebesthosting.com/plesk-plugin/node_modules/html-webpack-plugin/lib/loader.js):
-  Error: ENOENT: no such file or directory, open '/Users/bobi/PhpstormProjects/takebesthosting.com/plesk-plugin/node_modules/@plesk/plesk-ext-sdk/lib/templates/index.ejs':
-  Error: ENOENT: no such file or directory, open '/Users/bobi/PhpstormProjects/takebesthosting.com/plesk-plugin/node_modules/@plesk/plesk-ext-sdk/lib/templates/index.ejs'
-  
-  - compiler.js:79 
-    [plesk-plugin]/[html-webpack-plugin]/lib/compiler.js:79:16
-  
-  - Compiler.js:343 
-    [plesk-plugin]/[webpack]/lib/Compiler.js:343:11
-  
-  - Compiler.js:681 
-    [plesk-plugin]/[webpack]/lib/Compiler.js:681:15
-  
-  
-  - Hook.js:154 AsyncSeriesHook.lazyCompileHook
-    [plesk-plugin]/[tapable]/lib/Hook.js:154:20
-  
-  - Compiler.js:678 
-    [plesk-plugin]/[webpack]/lib/Compiler.js:678:31
-  
-  
-  - Hook.js:154 AsyncSeriesHook.lazyCompileHook
-    [plesk-plugin]/[tapable]/lib/Hook.js:154:20
-  
-  - Compilation.js:1423 
-    [plesk-plugin]/[webpack]/lib/Compilation.js:1423:35
-  
-  
-</pre>
+<?php
+/**
+ * Microweber auto provision plesk plugin
+ * Author: Bozhidar Slaveykov
+ * @email: info@microweber.com
+ * Copyright: Microweber CMS
+ */
+
+$application = new pm_Application();
+$application->run();
