d299f2400c3f17e5120603bd4b06e83d3edbe138
Bozhidar Slaveykov <bobi@microweber.com>
2022-07-04 20:06:20 +0300

    add vendor

---
+++ plib/vendor/composer/InstalledVersions.php
@@ -0,0 +1,429 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Autoload\ClassLoader;
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '14c1453af7045e29c01c5f6a597d291e9cd20489',
+    'name' => 'microweber-dev/plesk-plugin',
+  ),
+  'versions' => 
+  array (
+    'microweber-dev/plesk-plugin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14c1453af7045e29c01c5f6a597d291e9cd20489',
+    ),
+    'microweber-packages/composer-client' => 
+    array (
+      'pretty_version' => '1.9',
+      'version' => '1.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f7a3d4343c501e33ffc8e47e65d8155e218e7bbf',
+    ),
+    'microweber-packages/shared-server-scripts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'e21ce36b9671a3978d18f6735ca23f80e8a20516',
+    ),
+    'plesk/pm-api-stubs' => 
+    array (
+      'pretty_version' => '18.0.44',
+      'version' => '18.0.44.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eebdfd895f8633b667ed1bfa2020e616eb4cd8af',
+    ),
+    'plesk/zf1' => 
+    array (
+      'pretty_version' => '1.12.17-patch4',
+      'version' => '1.12.17.0-patch4',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c9e09b1fbc0e18e302211bd90f72408d01dbb24a',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '513e0666f7216c7459170d56df27dfcefe1689ea',
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-server-handler' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aff2f80e33b7f026ec96bb42f63242dc50ffcae7',
+    ),
+    'psr/http-server-middleware' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2296f45510945530b9dceb8bcedb5cb84d40c5f5',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'symfony/filesystem' => 
+    array (
+      'pretty_version' => 'v5.4.9',
+      'version' => '5.4.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '36a017fa4cce1eff1b8e8129ff53513abcef05ba',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6fd1b9a79f6e3cf65f9e679b23af304cd9e010d4',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9344f9cb97f3b19424af1a21a3b0e75b0a7d8d7e',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cfa0ae98841b9e461207c13ab093d76b0fa7bace',
+    ),
+    'symfony/process' => 
+    array (
+      'pretty_version' => 'v5.4.8',
+      'version' => '5.4.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '597f3fff8e3e91836bb0bd38f5718b56ddbde2f3',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.4.9',
+      'version' => '5.4.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af52239a330fafd192c773795520dc2dd62b5657',
+    ),
+  ),
+);
+private static $canGetVendors;
+private static $installedByVendor = array();
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+$packages = array();
+foreach (self::getInstalled() as $installed) {
+$packages[] = array_keys($installed['versions']);
+}
+
+
+if (1 === \count($packages)) {
+return $packages[0];
+}
+
+return array_keys(array_flip(\call_user_func_array('array_merge', $packages)));
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (isset($installed['versions'][$packageName])) {
+return true;
+}
+}
+
+return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+$ranges = array();
+if (isset($installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = $installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['pretty_version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['reference'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+$installed = self::getInstalled();
+
+return $installed[0]['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+self::$installedByVendor = array();
+}
+
+
+
+
+private static function getInstalled()
+{
+if (null === self::$canGetVendors) {
+self::$canGetVendors = method_exists('Composer\Autoload\ClassLoader', 'getRegisteredLoaders');
+}
+
+$installed = array();
+
+if (self::$canGetVendors) {
+
+ foreach (ClassLoader::getRegisteredLoaders() as $vendorDir => $loader) {
+if (isset(self::$installedByVendor[$vendorDir])) {
+$installed[] = self::$installedByVendor[$vendorDir];
+} elseif (is_file($vendorDir.'/composer/installed.php')) {
+$installed[] = self::$installedByVendor[$vendorDir] = require $vendorDir.'/composer/installed.php';
+}
+}
+}
+
+$installed[] = self::$installed;
+
+return $installed;
+}
+}
---
+++ plib/vendor/plesk/pm-api-stubs/pm/DomainAlias.php
@@ -0,0 +1,105 @@
+<?php
+// Copyright 1999-2022. Plesk International GmbH. All rights reserved.
+/**
+ * Domain alias wrapper
+ *
+ * @package Plesk_Modules
+ * @since 18.0.33
+ */
+class pm_DomainAlias
+{
+
+    /**
+     * Create new domain alias wrapper
+     *
+     * @param int $domainAliasId
+     * @throws pm_Exception
+     */
+    public function __construct(int $domainAliasId) { }
+
+    /**
+     * Retrieve domain aliases for the specified domain
+     *
+     * @param pm_Domain $domain
+     * @throws pm_Exception
+     *
+     * @return pm_DomainAlias[]
+     * @since 18.0.42
+     */
+    public static function getByDomain(pm_Domain $domain): array { }
+
+    /**
+     * Retrieve domain alias ID.
+     *
+     * @return int
+     */
+    public function getId(): int { }
+
+    /**
+     * Retrieves domain alias name in ASCII format
+     *
+     * @return string
+     * @since 18.0.42
+     */
+    public function getName(): string { }
+
+    /**
+     * Retrieves domain name as it is displayed in UI
+     *
+     * @return string
+     * @since 18.0.42
+     */
+    public function getDisplayName(): string { }
+
+    /**
+     * Shows whether web hosting is enabled on the domain alias
+     *
+     * @return bool
+     * @since 18.0.42
+     */
+    public function hasServiceWeb(): bool { }
+
+    /**
+     * Shows whether DNS service is enabled on the domain alias
+     *
+     * @return bool
+     * @since 18.0.42
+     */
+    public function hasServiceDns(): bool { }
+
+    /**
+     * Shows whether mail service is enabled on the domain alias
+     *
+     * @return bool
+     * @since 18.0.42
+     */
+    public function hasServiceMail(): bool { }
+
+    /**
+     * Shows whether 301 redirect to HTTPS is enabled on the domain alias
+     *
+     * @return bool
+     * @since 18.0.42
+     */
+    public function hasSeoRedirect(): bool { }
+
+    /**
+     * Retrieve domain alias by known id
+     *
+     * @param int $domainAliasId
+     * @throws pm_Exception
+     *
+     * @return pm_DomainAlias
+     */
+    public static function getByDomainAliasId(int $domainAliasId): pm_DomainAlias { }
+
+    /**
+     * Return DNS zone of domain alias
+     *
+     * @throws pm_Exception
+     *
+     * @return pm_Dns_Zone
+     */
+    public function getDnsZone(): pm_Dns_Zone { }
+
+}
