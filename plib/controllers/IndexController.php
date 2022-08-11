855749e5a0eb64afff8d6dbad823e7cb7fa87f22
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-11 11:55:28 +0300

    Update IndexController.php

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -1658,7 +1658,7 @@ class IndexController extends Modules_Microweber_BasepluginController
                         'app_version' => $installation['appVersion'],
                         'document_root' => $installation['appInstallation'],
                         'active' => ($installation['domainIsActive'] ? 'Yes' : 'No'),
-                        'action' => '<div style="color:#f66e6e;">Error when installing the application.</div><a class="btn btn-info">Remove</a>&nbsp;&nbsp;<a class="btn btn-info">Open Error Log</a>'
+                        'action' => '<div style="color:#f66e6e;">Error when installing the application.</div><a onclick="removeDomainAppInstallation(this)" class="btn btn-info">Remove</a>&nbsp;&nbsp;<a class="btn btn-info">Open Error Log</a>'
                     ];
 
                     $installationsCount++;
