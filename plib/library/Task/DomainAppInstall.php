89d483c2a37aefc7a3bbd5d386bf20fae342e4a4
Bozhidar Slaveykov <bobi@microweber.com>
2024-01-16 11:32:39 +0200

    Update DomainAppInstall.php

--- plib/library/Task/DomainAppInstall.php
+++ plib/library/Task/DomainAppInstall.php
@@ -19,30 +19,30 @@ class Modules_Microweber_Task_DomainAppInstall extends \pm_LongTask_Task
 		//$newInstallation->setDatabaseServerId($this->getParam('databaseServerId'));
 		$newInstallation->setPath($this->getParam('path'));
 
-        if (!empty($this->getParam('ssl'))) { 
+        if (!empty($this->getParam('ssl'))) {
             $newInstallation->setSsl($this->getParam('ssl'));
         }
 
 		if (!empty($this->getParam('email'))) {
 			$newInstallation->setEmail($this->getParam('email'));
 		}
-		
+
 		if (!empty($this->getParam('username'))) {
 			$newInstallation->setUsername($this->getParam('username'));
 		}
-		
+
 		if (!empty($this->getParam('password'))) {
 			$newInstallation->setPassword($this->getParam('password'));
 		}
-		
+
 		if (!empty($this->getParam('template'))) {
 			$newInstallation->setTemplate($this->getParam('template'));
 		}
-		
+
 		if (!empty($this->getParam('language'))) {
 			$newInstallation->setLanguage($this->getParam('language'));
 		}
-		
+
 		$newInstallation->setProgressLogger($this);
 		$status = $newInstallation->run();
 
@@ -67,28 +67,30 @@ class Modules_Microweber_Task_DomainAppInstall extends \pm_LongTask_Task
 
 	public function statusMessage()
 	{
-		switch ($this->getStatus()) {
+        if (pm_Settings::get('installation_notifications') == 'yes') {
 
-			case static::STATUS_RUNNING:
+            switch ($this->getStatus()) {
 
-				return 'Installing '.Modules_Microweber_WhiteLabel::getBrandName().' on ' .  $this->getParam('domainDisplayName', 'none') .'/'.$this->getParam('path');
+                case static::STATUS_RUNNING:
+                    return 'Installing ' . Modules_Microweber_WhiteLabel::getBrandName() . ' on ' . $this->getParam('domainDisplayName', 'none') . '/' . $this->getParam('path');
 
-			case static::STATUS_DONE:
+                case static::STATUS_DONE:
 
-				return ''.Modules_Microweber_WhiteLabel::getBrandName().' is installed successfully on ' . $this->getParam('domainDisplayName', 'none') .'/'.$this->getParam('path');
+                    return '' . Modules_Microweber_WhiteLabel::getBrandName() . ' is installed successfully on ' . $this->getParam('domainDisplayName', 'none') . '/' . $this->getParam('path');
 
-			case static::STATUS_ERROR:
+                case static::STATUS_ERROR:
 
-				return 'Error installing '.Modules_Microweber_WhiteLabel::getBrandName().' on ' . $this->getParam('domainDisplayName', 'none') .'/'.$this->getParam('path');
+                    return 'Error installing ' . Modules_Microweber_WhiteLabel::getBrandName() . ' on ' . $this->getParam('domainDisplayName', 'none') . '/' . $this->getParam('path');
 
-			case static::STATUS_NOT_STARTED:
+                case static::STATUS_NOT_STARTED:
 
-				return pm_Locale::lmsg('taskPingError', [
-					'id' => $this->getId()
-				]);
-		}
+                    return pm_Locale::lmsg('taskPingError', [
+                        'id' => $this->getId()
+                    ]);
+            }
+        }
 
-		return '';
+		return false;
 	}
 
 	public function onStart()
