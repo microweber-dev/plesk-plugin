5bf9ff30c4705426c5ecad4e12893d79995c808f
Bozhidar Slaveykov <bobi@microweber.com>
2024-02-15 16:13:40 +0200

    Update UpdateDomainsPhpHandler.php

--- plib/library/Task/UpdateDomainsPhpHandler.php
+++ plib/library/Task/UpdateDomainsPhpHandler.php
@@ -71,13 +71,4 @@ class Modules_Microweber_Task_UpdateDomainsPhpHandler extends \pm_LongTask_Task
         return '';
     }
 
-    public function onStart()
-    {
-        $this->setParam('onStart', 1);
-    }
-
-    public function onDone()
-    {
-        $this->setParam('onDone', 1);
-    }
 }
