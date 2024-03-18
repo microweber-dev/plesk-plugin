ace458311d9bfbb0e72d92fc5b13c444db149ed8
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-18 17:13:42 +0200

    Update startup.js

--- htdocs/js/startup.js
+++ htdocs/js/startup.js
@@ -1,6 +1,28 @@
 var $j = jQuery.noConflict();
 $j(document).ready(function() {
 
+    function getLongTaskStatus() {
+        $j.get('/modules/microweber/index.php/task/taskstatuses', function (data) {
+            console.log(data);
+            if (data.app_download) {
+                if (data.app_download.status == 'done') {
+                    window.location.href = '/modules/microweber/index.php/index';
+                }
+            }
+            if (data.templates_download) {
+                if (data.templates_download.status == 'running') {
+                    $j('.js-download-app').attr('disabled','disabled');
+                    $j('.js-download-app').html('Installing templates...');
+                }
+                if (data.templates_download.status == 'done') {
+                    window.location.href = '/modules/microweber/index.php/index';
+                }
+            }
+        });
+    }
+
+    getLongTaskStatus();
+
     $j('body').on('click', '.js-download-app', function() {
 
         $j('.js-download-app').attr('disabled','disabled');
