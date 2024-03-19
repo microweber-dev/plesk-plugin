f22d3fe0781791902b2174c89402ae6ed0d0c86b
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-19 11:08:36 +0200

    update

--- htdocs/js/versions.js
+++ htdocs/js/versions.js
@@ -3,11 +3,16 @@ $j(document).ready(function() {
 
     function getLongTaskStatus() {
         $j.get('/modules/microweber/index.php/task/taskstatuses', function (data) {
+            let showTemplateUpToDate = true;
             if (data.tasks.templates_download) {
                 if (data.tasks.templates_download.status == 'running') {
                     $j('.js-check-for-update').attr('disabled','disabled');
                     $j('.js-check-for-update').html('Installing templates...');
+                    showTemplateUpToDate = false;
                 }
+            }
+            if (data.app_installed && showTemplateUpToDate) {
+                $j('.js-templates-up-to-date').show();
             }
         });
     }
