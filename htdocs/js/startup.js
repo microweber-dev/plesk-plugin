4361d35c32aac537ef79014094aec6cd5305ef5a
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-19 14:53:55 +0200

    Update startup.js

--- htdocs/js/startup.js
+++ htdocs/js/startup.js
@@ -3,45 +3,48 @@ $j(document).ready(function() {
 
     function getLongTaskStatus(callback = null) {
         $j.get('/modules/microweber/index.php/task/taskstatuses', function (data) {
+
+            let templatesDownloadStatus = false;
+            let appDownloadStatus = false;
             if (data.tasks.templates_download) {
                 if (data.tasks.templates_download.status == 'running') {
                     $j('.js-download-app').attr('disabled','disabled');
                     $j('.js-download-app').html('Installing templates...');
+                    templatesDownloadStatus = 'running';
                 }
             }
             if (data.tasks.app_download) {
                 if (data.tasks.app_download.status == 'running') {
                     $j('.js-download-app').attr('disabled', 'disabled');
                     $j('.js-download-app').html('Installing app...');
+                    appDownloadStatus = 'running';
                 }
             }
             if (data.app_installed && data.tasks.app_download.status == 'done') {
                 window.location.href = '/modules/microweber/index.php/index';
             }
-            if (callback) {
-                callback(data);
-            }
+            callback({
+                templatesDownloadStatus: templatesDownloadStatus,
+                appDownloadStatus: appDownloadStatus,
+            });
         });
     }
 
     getLongTaskStatus((getLongTaskStatuses)=> {
         if (getLongTaskStatuses) {
             // Some tasks are running
-            if (typeof getLongTaskStatuses.tasks.templates_download != 'undefined' ||
-                typeof getLongTaskStatuses.tasks.app_download != 'undefined') {
-                if (
-                    getLongTaskStatuses.tasks.templates_download.status == 'running' ||
-                    getLongTaskStatuses.tasks.app_download.status == 'running'
-                ) {
-                    let longTaskStatusInterval = setInterval(function () {
-                        getLongTaskStatus(checkStatus => {
-                            if (checkStatus.tasks.templates_download.status !== 'running'
-                                && checkStatus.tasks.app_download.status !== 'running') {
-                                clearInterval(longTaskStatusInterval);
-                            }
-                        });
-                    }, 3000);
-                }
+            if (
+                (getLongTaskStatuses.templatesDownloadStatus == 'running') ||
+                (getLongTaskStatuses.appDownloadStatus == 'running')
+            ) {
+                let longTaskStatusInterval = setInterval(function () {
+                    getLongTaskStatus(checkStatus => {
+                        if (checkStatus.templatesDownloadStatus !== 'running'
+                            && checkStatus.appDownloadStatus !== 'running') {
+                            clearInterval(longTaskStatusInterval);
+                        }
+                    });
+                }, 3000);
             }
         }
     });
