f4c7e90ceefcd1afc16af03e64b067fc4164d6a5
Bozhidar Slaveykov <bobi@microweber.com>
2024-03-20 13:32:07 +0200

    Update download_file.sh

--- sbin/download_file.sh
+++ sbin/download_file.sh
@@ -13,5 +13,19 @@ zipDownloadedFile="$3";
 
 echo "Download from url:" "$downloadUrl";
 
+#wget "$downloadUrl" -O "$zipDownloadedFile"
 
-wget "$downloadUrl" -O "$zipDownloadedFile"
+# Retry 100 times
+Retry=0
+until wget "$downloadUrl" -O "$zipDownloadedFile"; do
+    printf 'DOWNLOAD....\n'
+    sleep 2
+    Retry=$((Retry+1))
+    if [ $Retry -eq 100 ]; then
+        echo "Failed to download file after 100 retries"
+        exit 1
+    fi
+done
+
+echo "Downloaded file:" "$zipDownloadedFile";
+
